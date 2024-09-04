968dff99056a52e7bbf9e63faf63a0bceca3b090
Mạnh Quân <95574783+NgMQ1111@users.noreply.github.com>
2024-09-04 19:22:52 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 provider "aws" {
-  shared_config_files = [ "C:/Users/Admin/.aws/config" ]
-  shared_credentials_files = [ "C:/Users/Admin/.aws/credentials" ]
-  profile = "010438467195_dev-quannm1096"
+  # shared_config_files = [ "C:/Users/Admin/.aws/config" ]
+  # shared_credentials_files = [ "C:/Users/Admin/.aws/credentials" ]
+  # profile = "010438467195_dev-quannm1096"
+  region = var.aws_region
 }
