7701f91ebc845f640f2f009382a74f7303a9f12a
Mạnh Quân <95574783+NgMQ1111@users.noreply.github.com>
2024-09-04 19:25:56 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "ap-southeast-2"
+}
+
 provider "aws" {
   # shared_config_files = [ "C:/Users/Admin/.aws/config" ]
   # shared_credentials_files = [ "C:/Users/Admin/.aws/credentials" ]
